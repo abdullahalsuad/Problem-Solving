c6dcbe51abec36d572901f654c6bb8866e10d5dc
Harun-Ar- Rashid <suadabdullahal@gmail.com>
2021-09-21 12:34:23 +0600

    2st prob

---
+++ problem 2.js
@@ -0,0 +1,6 @@
+// 2. How can you order roll numbers of students in your class sequentially?
+
+const rolls = ["12","14","1","50","30","40"]
+console.log(rolls.sort(function(a,b){
+      return a - b;
+}))
